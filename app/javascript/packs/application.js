c8e9f8590dfd586020baf7921c2d9a0d078d4115
ryota-5k <rk0509@hotmail.co.jp>
2021-06-06 17:40:27 +0900

    Implement basic login

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,17 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import 'bootstrap'
+import 'jquery'
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+// window.onload = function(){
+//   const dropdownMenu = document.querySelector(".dropdown-menu");
+//   const dropdownToggle = document.querySelector(".dropdown-toggle");
+//   dropdownToggle.addEventListener("click", function(){
+//     dropdownMenu.classList.toggle("dropdown_display")
+//   }),false
+// }
